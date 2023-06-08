edfd323111e19b08a1b01fc4aa1c7514fc1f52b2
SunInit <61983992+SunInit@users.noreply.github.com>
2023-06-08 21:01:06 +0200

    Add files via upload
    refactoring

--- Kutscher_Husted_Malze_U8_Nr2.cpp
+++ Kutscher_Husted_Malze_U8_Nr2.cpp
@@ -13,7 +13,7 @@ Faktor ca: 12,5
 private:
 
     double* daten;
-    
+
 public:
 
     /*
@@ -25,9 +25,6 @@ public:
 
         if (ps < 0) {
             ps = 0.1;
-        }
-        else if (ps > 0.7) {
-            ps = 0.7;
         }
         
         double akku = 100;
@@ -104,6 +101,19 @@ public:
         return this->daten[1] - this->daten[2];
     }
 
+     /*
+    Voraussetzung: Keine
+    Effekt: Alle aktuellen Daten auf Konsole ausgeben
+    Ergebnis: kein
+    */
+    void zeigeInfo() {
+        cout << "    PS: " 
+             << gibPS() << "\n    Akkuladung: " 
+             << gibAkku() << " % \n    gefahrene Strecke: " 
+             << gibKm() << " km \n    Reststrecke: " 
+             << gibRestKm() << " km" << endl;
+    }
+
     /*
     Voraussetzung: Eine Strecke in km eingeben
     Effekt: Falls mit der Restakkulaufzeit möglich wird die Strecke gefahren und die Daten aktualisiert.
@@ -113,9 +123,9 @@ public:
         if (gibRestKm() >= km) {
             this->daten[2] += km; // neue gesamt km
             this->daten[3] -= (100 * km) / this->daten[1]; // neue Restakku in %
-            cout << "Restakku %: " << gibAkku() << " Reststrecke km: " << gibRestKm() << endl;
+            cout << "\n Restakku: " << gibAkku() << " %, Reststrecke: " << gibRestKm() << " km" << endl;
         }
-        else {cout << "Nicht genügend Akku, bitte erst aufladen! Reststrecke möglich: " << gibRestKm() << endl;}
+        else {cout << "\n Nicht genügend Akku. Reststrecke möglich: " << gibRestKm() << " km" << endl;}
     }
 
     /*
@@ -126,6 +136,7 @@ public:
     void ladeAkku() {
         this->daten[3] = 100;
         this->daten[2] = 0;
+        cout << "\n Der Akku ist wieder voll geladen! \n";
     }
 };
 
@@ -137,38 +148,50 @@ void scooterFahren() {
 
     Escooter leihScooter = Escooter(0.3);
 
-    cout << "Wieviel PS hat dein Scooter? \n";
+    cout << "\n Wieviel PS hat dein Scooter? \n";
     cin >> ps;
     Escooter sc1 = Escooter(ps);
-    cout << "Dein Scooter hat " << sc1.gibPS() << " PS und damit eine Reichweite von " << sc1.gibRestKm() << " km \n";
-    // cout << "Unser Leihscooter hat " << leihScooter.gibPS() << " PS und wäre somit " << (sc1 < leihScooter) ? "stärker \n" : "schwächer \n";
+    sc1.zeigeInfo();
+
+    if (sc1 < leihScooter) {
+        cout << "\n Unser Leihscooter hat : \n";
+        leihScooter.zeigeInfo();
+    }
 
     while (funktion != 0) {
-        cout << "Was möchtest du tun? \n 1 - Deinen Scooter fahren \n 2 - Deine Scooter Daten ausgben \n 3 - Deinen Akku aufladen \n 4 - Leihscooter fahren \n 0 - Scooter fahren beenden? \n";
+        cout << "\n Was möchtest du tun? \n"
+             << " 1 - Deinen Scooter fahren \n"
+             << " 2 - Deine Scooter Daten ausgben \n" 
+             << " 3 - Deinen Akku aufladen \n"
+             << " 4 - Leihscooter fahren \n"
+             << " 0 - Scooter fahren beenden \n";
         cin >> funktion;
         if (funktion == 1) {
-            cout << "Wie weit soll gefahren werden? \n ";
+            cout << "\n Wie viele km soll gefahren werden? \n ";
             cin >> strecke;
             sc1.fahreKm(strecke);
             strecke = 0;
         }
         else if (funktion == 2) {
-            cout << "Deine Scooter Daten derzeit: \n    PS: " << sc1.gibPS() << "\n    Akkuladung in %: " << sc1.gibAkku() << "\n    gefahrene Strecke in Km: " << sc1.gibKm() << endl;
+            cout << "Deine Scooter Daten: \n";
+            sc1.zeigeInfo();
         }
         else if (funktion == 3) {
-            cout << "Der Akku ist wieder voll geladen! \n";
             sc1.ladeAkku();
         }
         else if (funktion == 4) {
-            cout << "Unser Leihscooter hat " << leihScooter.gibPS() << " PS und eine Reichweite von: " << leihScooter.gibRestKm() << "kM \n";
-            cout << "Wie weit soll gefahren werden? \n ";
-            cin >> strecke;
-            if (strecke <= leihScooter.gibRestKm()) {
-                leihScooter.fahreKm(strecke);
+            cout << "Unser Leihscooter hat: \n";
+            leihScooter.zeigeInfo();
+
+            while (leihScooter.gibRestKm() > 0) {
+
+                cout << "\n Wie viele km soll gefahren werden? \n Gib 0 ein für Rückgabe! \n ";
+                cin >> strecke;
+
+                if (strecke == 0) {cout << "\n Vielen Dank \n"; leihScooter.ladeAkku(); break;}
+
+                else {leihScooter.fahreKm(strecke); strecke = 0;}
             }
-            else {cout << "Leider schaffen unsere Scooter diese Strecke nicht! \n";}
-            leihScooter.ladeAkku(); //Darum muss sich der Kunde beim Leihscooter nicht selber kümmern...
-            strecke = 0;
         }
     }
     return;
@@ -180,7 +203,10 @@ int main () {
     unsigned auswahl = 100;
 
     while (auswahl != 0) {
-        cout << "Welche Simulation soll gestartet werden? 1 - Escooter, 2 - Kochfeld, 0 - Programm beenden \n";
+        cout << "\n Welche Simulation soll gestartet werden? \n"
+             << " 1 - E-Scooter \n"
+             << " 2 - Kochfeld \n"
+             << " 0 - Programm beenden \n";
         cin >> auswahl;
 
         if (auswahl == 1) {scooterFahren();}
